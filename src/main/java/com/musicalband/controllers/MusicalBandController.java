0c44d3adda28e586eed9d2b7ebfea3f916a9977f
shreyaagrawal281994 <shreya.agrawal281994@gmail.com>
2025-01-31 22:32:52 +0400

    Added Swagger Open API specification

--- src/main/java/com/musicalband/controllers/MusicalBandController.java
+++ src/main/java/com/musicalband/controllers/MusicalBandController.java
@@ -5,6 +5,7 @@ import com.musicalband.entities.MusicalBand;
 import com.musicalband.entities.Musician;
 import com.musicalband.entities.Person;
 import com.musicalband.services.MusicalBandService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,46 +20,55 @@ public class MusicalBandController {
     private final MusicalBandService musicalBandService;
 
     @PostMapping(value = "/person")
+    @Operation(summary = "Create a person")
     public Person createPerson(@RequestBody Person person) {
         return musicalBandService.createPerson(person);
     }
 
     @PostMapping(value = "/musician")
+    @Operation(summary = "Create a musician")
     public Musician createMusician(@RequestBody Musician musician) {
         return musicalBandService.createMusician(musician);
     }
 
     @PostMapping(value = "/band")
+    @Operation(summary = "Create a band")
     public Band createBand(@RequestBody Band band) {
         return musicalBandService.createBand(band);
     }
 
     @PostMapping(value = "/musical-band")
+    @Operation(summary = "Create a musical-band")
     public MusicalBand createMusicalBand(@RequestBody MusicalBand musicalBand) {
         return musicalBandService.createMusicalBand(musicalBand);
     }
 
     @GetMapping(value = "/persons")
+    @Operation(summary = "Retrieves the list of all the persons")
     public List<Person> getPersons() {
         return musicalBandService.getPersons();
     }
 
     @GetMapping(value = "/musicians")
+    @Operation(summary = "Retrieves the list of all the musicians")
     public List<Musician> getMusicians() {
         return musicalBandService.getMusicians();
     }
 
     @GetMapping(value = "/bands")
+    @Operation(summary = "Retrieves the list of all the bands")
     public List<Band> getBands() {
         return musicalBandService.getBands();
     }
 
     @GetMapping(value = "/musical-bands")
+    @Operation(summary = "Retrieves the list of all the musical-bands")
     public List<MusicalBand> getMusicalBands() {
         return musicalBandService.getMusicalBands();
     }
 
     @GetMapping(value = "/person/{id}")
+    @Operation(summary = "Retrieve person by id")
     public Person findByPersonId(@PathVariable(value = "id") Long id) {
         return musicalBandService.getPersonById(id).get();
     }
